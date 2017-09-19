216d7eec57a6066cae519061a65038c7cadd7f33
hanhonglei <hanhonglei@cuc.edu.cn>
2017-09-20 07:54:37 +0800

    no message

--- Samples/GLUTPhysX/GlutPhysX/GlutPhysX/GlutPhysX.cpp
+++ Samples/GLUTPhysX/GlutPhysX/GlutPhysX/GlutPhysX.cpp
@@ -3,8 +3,48 @@
 
 #include "stdafx.h"
 #include "freeglut\include\GL\freeglut.h"
+#include "foundation/PxFoundationVersion.h"
+#include "foundation/Px.h"
+#include "extensions/PxDefaultAllocator.h"
 
-//#pragma comment(lib, "freeglut\lib\x64\freeglut.lib")
+#include "PxPhysXConfig.h"
+#include "foundation/PxMemory.h"
+#include "extensions/PxDefaultErrorCallback.h"
+#include "cudamanager/PxCudaContextManager.h"
+#include "gpu/PxGpu.h"
+#include "PxPhysicsVersion.h"
+#include "PxPhysics.h"
+#include "common/PxTolerancesScale.h"
+#include "PxSceneDesc.h"
+#include "extensions/PxDefaultCpuDispatcher.h"
+#include "cooking/pxcooking.h"
+
+//#include "PxTkFile.h"
+//#include "PsString.h"
+//#include "PsFPU.h"
+//
+//#include "PxToolkit.h"
+#include "extensions/PxDefaultStreams.h"
+
+#pragma comment(lib, "freeglut.lib")
+
+using namespace physx;
+
+#if PX_WINDOWS
+// Starting with Release 302 drivers, application developers can direct the Optimus driver at runtime to use the High Performance
+// Graphics to render any application; even those applications for which there is no existing application profile.
+// They can do this by exporting a global variable named PxOptimusEnablement.
+// A value of 0x00000001 indicates that rendering should be performed using High Performance Graphics.
+// A value of 0x00000000 indicates that this method should be ignored.
+extern "C"
+{
+	_declspec(dllexport) DWORD PxOptimusEnablement = 0x00000001;
+}
+#endif
+
+static bool gRecook = false;
+PxDefaultAllocator gDefaultAllocatorCallback;
+
 
 const int	WINDOW_WIDTH = 1024,
 WINDOW_HEIGHT = 768;
@@ -122,7 +162,259 @@ void DrawGrid(int GRID_SIZE)
 //	}
 //}
 //
-//void InitializePhysX() {
+void InitializePhysX() {
+
+	//Recording memory allocations is necessary if you want to 
+	//use the memory facilities in PVD effectively.  Since PVD isn't necessarily connected
+	//right away, we add a mechanism that records all outstanding memory allocations and
+	//forwards them to PVD when it does connect.
+
+	//This certainly has a performance and memory profile effect and thus should be used
+	//only in non-production builds.
+	bool recordMemoryAllocations = true;
+#ifdef RENDERER_ANDROID
+	const bool useCustomTrackingAllocator = false;
+#else
+	const bool useCustomTrackingAllocator = true;
+#endif
+
+	PxAllocatorCallback* allocator = &gDefaultAllocatorCallback;
+
+	//if (useCustomTrackingAllocator)
+	//	allocator = getSampleAllocator();		//optional override that will track memory allocations
+
+	static PxDefaultErrorCallback gDefaultErrorCallback;
+	
+	PxFoundation *mFoundation = PxCreateFoundation(PX_FOUNDATION_VERSION, *allocator, gDefaultErrorCallback/*getSampleErrorCallback()*/);
+	//if (!mFoundation)
+	//	fatalError("PxCreateFoundation failed!");
+
+	bool mCreateCudaCtxManager = true;
+
+#if PX_SUPPORT_GPU_PHYSX
+	if (mCreateCudaCtxManager)
+	{
+		PxCudaContextManagerDesc cudaContextManagerDesc;
+
+#if defined(RENDERER_ENABLE_CUDA_INTEROP)
+		if (!mApplication.getCommandLine().hasSwitch("nointerop"))
+		{
+			switch (getRenderer()->getDriverType())
+			{
+			case Renderer::DRIVER_DIRECT3D11:
+				cudaContextManagerDesc.interopMode = PxCudaInteropMode::D3D11_INTEROP;
+				break;
+			case Renderer::DRIVER_OPENGL:
+				cudaContextManagerDesc.interopMode = PxCudaInteropMode::OGL_INTEROP;
+				break;
+			}
+			cudaContextManagerDesc.graphicsDevice = getRenderer()->getDevice();
+		}
+#endif 
+		PxCudaContextManager* mCudaContextManager = PxCreateCudaContextManager(*mFoundation, cudaContextManagerDesc);
+		if (mCudaContextManager)
+		{
+			if (!mCudaContextManager->contextIsValid())
+			{
+				mCudaContextManager->release();
+				mCudaContextManager = NULL;
+			}
+		}
+	}
+#endif
+
+	//createPvdConnection();
+
+	PxTolerancesScale scale;
+	//customizeTolerances(scale);
+
+	PxPhysics *mPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, scale, recordMemoryAllocations);
+	//if (!mPhysics)
+	//	fatalError("PxCreatePhysics failed!");
+
+	//if (!PxInitExtensions(*mPhysics, mPvd))
+	//	fatalError("PxInitExtensions failed!");
+
+	PxCookingParams params(scale);
+	//params.meshWeldTolerance = 0.001f;
+	//params.meshPreprocessParams = PxMeshPreprocessingFlags(PxMeshPreprocessingFlag::eWELD_VERTICES);
+	//params.buildGPUData = true; //Enable GRB data being produced in cooking.
+	PxCooking *mCooking = PxCreateCooking(PX_PHYSICS_VERSION, *mFoundation, params);
+	//if (!mCooking)
+	//	fatalError("PxCreateCooking failed!");
+
+	//mPhysics->registerDeletionListener(*this, PxDeletionEventFlag::eUSER_RELEASE);
+
+	//// setup default material...
+	//mMaterial = mPhysics->createMaterial(0.5f, 0.5f, 0.1f);
+	//if (!mMaterial)
+	//	fatalError("createMaterial failed!");
+
+#if defined(RENDERER_TABLET)
+	// load touchscreen control material
+	{
+		SampleFramework::SampleAsset* ps_asset = getAsset(controlMaterialPath, SampleFramework::SampleAsset::ASSET_MATERIAL);
+		mManagedAssets.push_back(ps_asset);
+		PX_ASSERT(ps_asset->getType() == SampleFramework::SampleAsset::ASSET_MATERIAL);
+		SampleFramework::SampleMaterialAsset* mat_ps_asset = static_cast<SampleFramework::SampleMaterialAsset*>(ps_asset);
+		if (mat_ps_asset->getNumVertexShaders() > 0)
+		{
+			RenderMaterial* mat = SAMPLE_NEW(RenderMaterial)(*getRenderer(), mat_ps_asset->getMaterial(0), mat_ps_asset->getMaterialInstance(0), MATERIAL_CONTROLS);
+			mRenderMaterials.push_back(mat);
+		}
+	}
+	// load touchscreen button material
+	{
+		SampleFramework::SampleAsset* ps_asset = getAsset(buttonMaterialPath, SampleFramework::SampleAsset::ASSET_MATERIAL);
+		mManagedAssets.push_back(ps_asset);
+		PX_ASSERT(ps_asset->getType() == SampleFramework::SampleAsset::ASSET_MATERIAL);
+		SampleFramework::SampleMaterialAsset* mat_ps_asset = static_cast<SampleFramework::SampleMaterialAsset*>(ps_asset);
+		if (mat_ps_asset->getNumVertexShaders() > 0)
+		{
+			RenderMaterial* mat = SAMPLE_NEW(RenderMaterial)(*getRenderer(), mat_ps_asset->getMaterial(0), mat_ps_asset->getMaterialInstance(0), MATERIAL_BUTTONS);
+			mRenderMaterials.push_back(mat);
+		}
+	}
+	Renderer* renderer = getRenderer();
+	RenderMaterial* controlMaterial = getMaterial(MATERIAL_CONTROLS);
+	renderer->initControls(controlMaterial->mRenderMaterial,
+		controlMaterial->mRenderMaterialInstance);
+	RenderMaterial* buttonMaterial = getMaterial(MATERIAL_BUTTONS);
+	// add buttons for common use
+	PxReal yInc = -0.12f;
+	PxVec2 leftBottom(0.58f, 0.90f);
+	PxVec2 rightTop(0.99f, 0.82f);
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+
+	// add buttons for individual sample
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+
+	// quick access button
+	leftBottom.y += yInc; rightTop.y += yInc;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+
+	// next, previous buttons
+	leftBottom.x = -0.4f;
+	leftBottom.y = 0.9f;
+	rightTop.x = -0.1f;
+	rightTop.y = 0.82f;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+	leftBottom.x = 0.1f;
+	leftBottom.y = 0.9f;
+	rightTop.x = 0.4f;
+	rightTop.y = 0.82f;
+	renderer->addButton(leftBottom, rightTop, NULL,
+		buttonMaterial->mRenderMaterial, buttonMaterial->mRenderMaterialInstance);
+
+#endif		
+
+	PX_ASSERT(NULL == mScene);
+
+	PxSceneDesc sceneDesc(mPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	//getDefaultSceneDesc(sceneDesc);
+	PxDefaultCpuDispatcher*					mCpuDispatcher;
+
+
+	if (!sceneDesc.cpuDispatcher)
+	{
+		mCpuDispatcher = PxDefaultCpuDispatcherCreate(1);
+		//if (!mCpuDispatcher)
+		//	fatalError("PxDefaultCpuDispatcherCreate failed!");
+		sceneDesc.cpuDispatcher = mCpuDispatcher;
+	}
+
+	if (!sceneDesc.filterShader)
+		sceneDesc.filterShader = getSampleFilterShader();
+
+#if PX_SUPPORT_GPU_PHYSX
+	if (!sceneDesc.gpuDispatcher && mCudaContextManager)
+		sceneDesc.gpuDispatcher = mCudaContextManager->getGpuDispatcher();
+#endif
+
+	//sceneDesc.frictionType = PxFrictionType::eTWO_DIRECTIONAL;
+	//sceneDesc.frictionType = PxFrictionType::eONE_DIRECTIONAL;
+	sceneDesc.flags |= PxSceneFlag::eENABLE_GPU_DYNAMICS;
+	sceneDesc.flags |= PxSceneFlag::eENABLE_PCM;
+	//sceneDesc.flags |= PxSceneFlag::eENABLE_AVERAGE_POINT;
+	sceneDesc.flags |= PxSceneFlag::eENABLE_STABILIZATION;
+	//sceneDesc.flags |= PxSceneFlag::eADAPTIVE_FORCE;
+	sceneDesc.flags |= PxSceneFlag::eENABLE_ACTIVETRANSFORMS;
+	sceneDesc.flags |= PxSceneFlag::eSUPPRESS_EAGER_SCENE_QUERY_REFIT;
+	//sceneDesc.flags |= PxSceneFlag::eDISABLE_CONTACT_CACHE;
+	sceneDesc.broadPhaseType = PxBroadPhaseType::eGPU;
+	sceneDesc.gpuMaxNumPartitions = 8;
+
+
+#ifdef USE_MBP
+	sceneDesc.broadPhaseType = PxBroadPhaseType::eMBP;
+#endif
+
+	customizeSceneDesc(sceneDesc);
+
+	mScene = mPhysics->createScene(sceneDesc);
+	if (!mScene)
+		fatalError("createScene failed!");
+
+	PxSceneWriteLock scopedLock(*mScene);
+
+	PxSceneFlags flag = mScene->getFlags();
+
+	PX_UNUSED(flag);
+	mScene->setVisualizationParameter(PxVisualizationParameter::eSCALE, mInitialDebugRender ? mDebugRenderScale : 0.0f);
+	mScene->setVisualizationParameter(PxVisualizationParameter::eCOLLISION_SHAPES, 1.0f);
+
+	PxPvdSceneClient* pvdClient = mScene->getScenePvdClient();
+	if (pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+
+#ifdef USE_MBP
+	setupMBP(*mScene);
+#endif
+
+	mApplication.refreshVisualizationMenuState(PxVisualizationParameter::eCOLLISION_SHAPES);
+	mApplication.applyDefaultVisualizationSettings();
+	mApplication.setMouseCursorHiding(false);
+	mApplication.setMouseCursorRecentering(false);
+	mCameraController.setMouseLookOnMouseButton(false);
+	mCameraController.setMouseSensitivity(1.0f);
+
+	if (mCreateGroundPlane)
+		createGrid();
+
+	LOG_INFO("PhysXSample", "Init sample ok!");
+}
 //	gPhysicsSDK = PxCreatePhysics(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback, PxTolerancesScale());
 //	if (gPhysicsSDK == NULL) {
 //		cerr << "Error creating PhysX3 device." << endl;
