dba2fdb1d1e3a513ac2a63fb504f559a943fff58
hanhonglei <hanhonglei@cuc.edu.cn>
2017-11-09 13:13:56 +0800

    Character controller

---
+++ Samples/GLUTPhysX - CCT/GlutPhysX/Graphics.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "stdafx.h"
+
+// Render font
+void SetOrthoForFont(int w, int h);
+
+void ResetPerspectiveProjection();
+void RenderSpacedBitmapString(
+	int x,
+	int y,
+	int spacing,
+	void *font,
+	char *string);
+
+void DrawAxes();
+
+void DrawGrid(int GRID_SIZE);
+
+void renderGeometry(const PxGeometryHolder& h);
+
+
+void setupDefaultRenderState();
+
+
+void renderActors(physx::PxRigidActor** actors, const physx::PxU32 numActors, bool shadows = false, const physx::PxVec3 & color = physx::PxVec3(0.0f, 0.75f, 0.0f));
+
+void RenderPhysXScene();
+
+
+void InitGL();
+
+void OnReshape(int nw, int nh);
+
+void OnRender();
---
+++ Samples/GLUTPhysX - CCT/GlutPhysX/Physics.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "stdafx.h"
+
+extern PxScene*				gScene;
+
+// Set up PhysX
+void InitializePhysX();
+
+void ShutdownPhysX();
+
+void stepPhysics(PxReal timeStep);
+void AddBullet(const PxVec3& pos, const PxVec3& v);
+void MoveCharacter(unsigned char key);
